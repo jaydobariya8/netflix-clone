2b9c6ca0929eced7ea113479cd18b89834d1ac84
jaydobariya8 <jaydobariya2341@gmail.com>
2021-09-26 13:19:28 +0530

    Doploy application on node server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
